396a3d14af92bb41170e6f3216dd48a4255e6960
ksiic <244992791@qq.com>
2018-02-12 17:53:18 +0800

    change web

---
+++ js/first.js
@@ -0,0 +1,31 @@
+
+// var canvas=document.getElementById('myConvas');
+// var ctx=canvas.getContext('2d');
+
+
+function open_win() 
+{
+	window.open("https://www.baidu.com");
+}
+
+// function msgalert() 
+// {
+// 	ctx.fillStyle='#FF0000';
+// 	ctx.fillRect(0,0,100,100);
+// 	alert("You clicked the Canvas.");
+// }
+// function mousemovein() 
+// {
+// 	//var canvasSize=120;
+// 	//canvas.width=canvasSize;
+// 	//canvas.height=canvasSize;
+// 	ctx.fillStyle='#710CD3';
+// 	ctx.fillRect(0,0,100,100);
+// 	//alert("You clicked the Canvas.");
+// }
+// function mouseLeave()
+// {
+// 	ctx.fillStyle='#ED13B6';
+// 	ctx.fillRect(0,0,100,100);
+// }
+
